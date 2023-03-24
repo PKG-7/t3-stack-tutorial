4881d3941c73b0c1ff9222975d3f473b7a99f795
PKG-7 <pavel.k.group@gmail.com>
2023-03-24 23:51:39 +0300

    router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
 
-  console.log(user);
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -22,6 +20,13 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>{!user.isSignedIn && <SignInButton />}</div>
         <div>{!!user.isSignedIn && <SignOutButton />}</div>
+        <div>
+          <div>
+            {data?.map((post) => (
+              <div key={post.id}>{post.content}</div>
+            ))}
+          </div>
+        </div>
       </main>
     </>
   );
