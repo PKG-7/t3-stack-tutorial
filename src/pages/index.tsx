45b242853acfd17087ae210d3e0dc2556b46e888
PKG-7 <pavel.k.group@gmail.com>
2023-03-25 11:16:32 +0300

    toasts, loading, enter press submit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,9 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 
 import { api, RouterOutputs } from "~/utils/api";
-import { LoadingPage } from "~/components/Loading";
+import { LoadingPage, LoadingSpinner } from "~/components/Loading";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -24,6 +25,15 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       ctx.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post! Please try again later");
+      }
     },
   });
 
@@ -44,9 +54,32 @@ const CreatePostWizard = () => {
         className="grow bg-transparent outline-none"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
         disabled={isPosting}
+        autoFocus
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {input !== "" && !isPosting && (
+        <button
+          className="rounded-xl bg-blue-500 px-4"
+          disabled={isPosting}
+          onClick={() => mutate({ content: input })}
+        >
+          Post
+        </button>
+      )}
+
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -65,7 +65,11 @@ export const postsRouter = createTRPCRouter({
   create: privateProcedure
     .input(
       z.object({
-        content: z.string().emoji().min(1).max(280),
+        content: z
+          .string()
+          .emoji("Можно постить только Эмодзи! Используйте win + .")
+          .min(1)
+          .max(280),
       })
     )
     .mutation(async ({ ctx, input }) => {
